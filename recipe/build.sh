ac12da1f0b451519ba67004ee15f3437b9f64133
Francesco Biscani <bluescarni@gmail.com>
2020-03-05 20:49:44 +0100

    Try disabling the -lrt workaround.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,6 @@ if [[ "$target_platform" == osx-64 ]]; then
     # Workaround for missing C++17 feature when building the tests.
     export CXXFLAGS="$CXXFLAGS -DCATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS"
 else
-    LDFLAGS="-lrt ${LDFLAGS}"
     export ENABLE_BACKTRACE=yes
 fi
 
